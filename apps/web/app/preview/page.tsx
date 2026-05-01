e578c4b26c17f7930248efe19c99ff29880d883a
Victon18 <abhinavbatham333@gmail.com>
2026-05-01 18:17:49 +0530

    preview error

--- apps/web/app/preview/page.tsx
+++ apps/web/app/preview/page.tsx
@@ -1,9 +1,7 @@
 import Hero from "@/components/Hero";
 import UseCases from "@/components/UseCases";
 import HowItWorks from "@/components/HowItWorks";
-import Testimonials from "@/components/Testimonials";
 import FAQ from "@/components/FAQ";
-import CTASection from "@/components/CTASection";
 import Footer from "@/components/Footer";
 
 export default function PreviewPage() {
@@ -13,9 +11,7 @@ export default function PreviewPage() {
         <Hero />
         <UseCases />
         <HowItWorks />
-        <Testimonials />
         <FAQ />
-        <CTASection />
       </main>
       <Footer />
     </div>
